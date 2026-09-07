9da95364ba7e79ae3c229d8ab653123593efd7a9
bbh0327l <kohhin2005@gmail.com>
2026-09-08 00:46:49 +0800

    Update loginPage.php, universityRegistartionPage.php, style.css

--- boundary/universityRegistrationPage.php
+++ boundary/universityRegistrationPage.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,14 +8,16 @@
     <title>Sign Up</title>
 
     <!-- CSS - External Sheet -->
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="../style.css">
 </head>
 
 <body>
 
     <!-- Header -->
     <header>
-        <div class="logo">UniBee</div>
+        <div class="logo-container">
+            <img src="../images/uniBeeLogo.png" alt="UniBee Logo">
+        </div>
         <div class="page-title">Sign up</div>
     </header>
 
@@ -32,78 +35,43 @@
                 <!-- Email -->
                 <div class="form-group">
                     <label for="email">Email address</label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        placeholder="Enter your email"
-                        required
-                    >
+                    <input type="email" id="email" name="email" placeholder="Enter your email" required>
                 </div>
 
 
                 <!-- Password -->
                 <div class="form-group">
                     <label for="password">Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        placeholder="Password"
-                        required
-                    >
+                    <input type="password" id="password" name="password" placeholder="Password" required>
                 </div>
 
 
                 <!-- University Name -->
                 <div class="form-group">
                     <label for="name">Name</label>
-                    <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        placeholder="Name"
-                        required
-                    >
+                    <input type="text" id="name" name="name" placeholder="Name" required>
                 </div>
 
 
                 <!-- Institution Type -->
                 <div class="form-group">
                     <label for="institutionType">Type of Institute</label>
-                    <input
-                        type="text"
-                        id="institutionType"
-                        name="institutionType"
-                        placeholder="Type of Institute"
-                        required
-                    >
+                    <input type="text" id="institutionType" name="institutionType" placeholder="Type of Institute"
+                        required>
                 </div>
 
 
                 <!-- Country -->
                 <div class="form-group">
                     <label for="country">Country</label>
-                    <input
-                        type="text"
-                        id="country"
-                        name="country"
-                        placeholder="Country"
-                        required
-                    >
+                    <input type="text" id="country" name="country" placeholder="Country" required>
                 </div>
 
 
                 <!-- Postal Code -->
                 <div class="form-group">
                     <label for="postalCode">Postal Code</label>
-                    <input
-                        type="text"
-                        id="postalCode"
-                        name="postalCode"
-                        placeholder="Postal Code"
-                        required
-                    >
+                    <input type="text" id="postalCode" name="postalCode" placeholder="Postal Code" required>
                 </div>
 
 
@@ -127,14 +95,20 @@
         <!-- Image -->
         <section class="registration-image">
 
-            <img
-                src=""
-                alt=""
-            >
+            <img src="" alt="">
 
         </section>
 
     </main>
+    <!-- Footer -->
+    <footer>
+        <div class="footer-bottom-bar">
+            &copy; 2026 UniBee. All rights reserved.
+        </div>
+    </footer>
+
+    <script src="../script.js"></script>
 
 </body>
+
 </html>
