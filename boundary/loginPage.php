76cf605a61b3836af580df55bb6a18d86fd19164
TQiHao <tohqihao@gmail.com>
2026-09-08 14:52:32 +0800

    Added CSS to login and resetpassword page

--- boundary/loginPage.php
+++ boundary/loginPage.php
@@ -22,67 +22,62 @@
     <!-- Login Section -->
     <main class="login-container">
 
-        <!-- Login Form -->
-        <section class="login-form">
+        <div class="login-wrapper">
 
-            <h1>Welcome back!</h1>
-            <p>Enter your Credentials to access your account</p>
+            <!-- Left Column: Login Form -->
+            <section class="login-form">
+                <h1>Welcome back!</h1>
+                <p>Enter your Credentials to access your account</p>
 
-            <!-- Display error messages if any -->
-            <?php
-            session_start();
-            if (isset($_SESSION['login_error'])) {
-                echo '<div class="error-message">' . $_SESSION['login_error'] . '</div>';
-                unset($_SESSION['login_error']);
-            }
-            if (isset($_SESSION['login_success'])) {
-                echo '<div class="success-message">' . $_SESSION['login_success'] . '</div>';
-                unset($_SESSION['login_success']);
-            }
-            ?>
+                <!-- Display error/success messages -->
+                <?php
+                session_start();
+                if (isset($_SESSION['login_error'])) {
+                    echo '<div class="error-message">' . $_SESSION['login_error'] . '</div>';
+                    unset($_SESSION['login_error']);
+                }
+                if (isset($_SESSION['login_success'])) {
+                    echo '<div class="success-message">' . $_SESSION['login_success'] . '</div>';
+                    unset($_SESSION['login_success']);
+                }
+                ?>
 
-            <form action="../controller/loginController.php" method="POST">
+                <form action="../controller/loginController.php" method="POST">
 
-                <!-- Email -->
-                <div class="form-group">
-                    <label for="email">Email address</label>
-                    <input type="email" id="email" name="email" placeholder="Enter your email" required>
-                </div>
+                    <div class="form-group">
+                        <label for="email">Email address</label>
+                        <input type="email" id="email" name="email" placeholder="Enter your email" required>
+                    </div>
 
-                <!-- Password -->
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" id="password" name="password" placeholder="Password" required>
-                </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" name="password" placeholder="Password" required>
+                    </div>
 
-                <!-- Remember Me -->
-                <div class="form-group">
-                    <input type="checkbox" id="remember" name="remember" value="1">
-                    <label for="remember">Remember for 30 days</label>
-                </div>
+                    <div class="checkbox-group">
+                        <input type="checkbox" id="remember" name="remember" value="1">
+                        <label for="remember">Remember for 30 days</label>
+                    </div>
 
-                <!-- Submit Button -->
-                <button type="submit" name="login">
-                    Login
-                </button>
+                    <button type="submit" name="login">Login</button>
 
-            </form>
+                </form>
 
-            <!-- Links -->
-            <p class="register-link">
-                Don't have an account?
-                <a href="universityRegistrationPage.php">Sign up</a>
-            </p>
-            <p class="reset-link">
-                <a href="resetPasswordPage.php">Forgot password?</a>
-            </p>
+                <p class="register-link">
+                    Don't have an account? <a href="universityRegistrationPage.php">Sign up</a>
+                </p>
+                <p class="reset-link">
+                    <a href="resetPasswordPage.php">Forgot password?</a>
+                </p>
 
-        </section>
+            </section>
 
-        <!-- Image Section (Optional) -->
-        <section class="login-image">
-            <img src="" alt="">
-        </section>
+            <!-- Right Column: Image -->
+            <section class="login-image">
+                <img src="../images/loginImage.png" alt="Campus Illustration">
+            </section>
+
+        </div>
 
     </main>
 
