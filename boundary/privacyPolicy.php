1038edc7deaf671f9c6d1e3f587de6255226f277
bbh0327l <kohhin2005@gmail.com>
2026-09-18 15:55:35 +0800

    Update privacyPolicy.php

--- boundary/privacyPolicy.php
+++ boundary/privacyPolicy.php
@@ -56,7 +56,8 @@ session_start();
                 18, 2026</p>
 
             <p style="margin-bottom: 20px;">
-                At <strong>UniBee</strong>, we prioritize the privacy and security of our students, lecturers, and
+                At <strong>UniBee</strong>, we prioritize the privacy and security of our students, lecturers, course
+                coordinators and
                 university administrators. This Privacy Policy outlines how UniBee collects, uses, and protects your
                 personal information across our smart campus services, modules, and mobile platform.
             </p>
